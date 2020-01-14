935d67c7d3ef215558ac824f1cbb5d99e534cb2a
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-01-14 16:15:59 +0000

    user decides to stay logged in or not via cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      # store their token on device if they want to be remembered
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # Create an error message.
@@ -17,7 +19,14 @@ class SessionsController < ApplicationController
 
   # Logout user and take them to home page
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+  
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,14 @@ module SessionsHelper
   
   # Returns the current logged-in user (if any).
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -18,7 +24,15 @@ module SessionsHelper
   
   # Logout current user and clear them from memory
   def log_out
+    forget(current_user)  # Forget user to prevent old cookie from logging in
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
